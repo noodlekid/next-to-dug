85bad243791706c0d44b27c551d3e3b7351f4fc0
Mateusz Tyc <tycmat9@gmail.com>
2024-11-22 22:54:12 -0500

    WIP: updated lost work

--- src/lib/managers/ConnectionManager.ts
+++ src/lib/managers/ConnectionManager.ts
@@ -6,6 +6,7 @@ export class ConnectionManager implements IConnectionManager {
     private rosConnection: IRosConnection | null = null;
     private connectionUrls: string[]= [];
     private currentUrlIndex: number = 0;
+    private reconnectionAttempts: number;
 
 
     public static getInstance(options?: any): ConnectionManager {
@@ -30,6 +31,14 @@ export class ConnectionManager implements IConnectionManager {
         this.establishedConnection(this.connectionUrls[this.currentUrlIndex]);
     }
 
+    public disconnect(): void {
+      if (this.rosConnection && this.isConnected()) {
+        console.log('Disconnecting from ROS...');
+        this.rosConnection.close();
+        // Stop hearbeat once I actually implement heart beat
+      }
+    }
+
     public isConnected(): boolean {
         return this.rosConnection ? this.rosConnection.isConnected(): false;
     }
@@ -42,6 +51,12 @@ export class ConnectionManager implements IConnectionManager {
         this.rosConnection.on('connection', this.handleConnection.bind(this));
         this.rosConnection.on('error', this.handleError.bind(this));
         this.rosConnection.on('close', this.handleClose.bind(this));
-      }    
+      }
+
+    private handleConnection(): void {
+        console.log('Connected to ROS.');
+        this.reconnectionAttempts = 0;
+        // Insert some other shit to handle heart beat
+    }
 
 }
